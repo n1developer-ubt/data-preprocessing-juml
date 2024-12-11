3cf636d8a3869681ddf9c5cb34ea295ecd646ec8
n1developer-ubt <u.b.t.usama.bin.tariq@gmail.com>
2024-12-11 11:15:24 +0100

    make file updated

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,12 @@
 using Documenter, PreprocessingPipeline
 
-makedocs()
+makedocs(
+    sitename = "PreprocessingPipeline.jl",
+    format = Documenter.HTML(),
+    pages = [
+        "Home" => "index.md"
+    ]
+)
 
 deploydocs(
     repo = "https://github.com/n1developer-ubt/data-preprocessing-juml.git",
