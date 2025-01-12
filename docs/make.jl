bf8b97acc1b92c381d58f8aa9dceffccb80f95c4
Maria <maria.cole@alice.de>
2025-01-12 18:43:10 +0100

    Autodoc updated again

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,21 @@
 using PreprocessingPipeline
 using Documenter 
 
-makedocs(
+makedocs(;
     modules=[PreprocessingPipeline],
     sitename = "PreprocessingPipeline.jl",
-    format = Documenter.HTML(),
+    format = Documenter.HTML(;
+        canonical="https://github.com/n1developer-ubt/data-preprocessing-juml",
+        edit_link="main",
+        assets=String[],
+    ),
     pages = [
         "Home" => "index.md"
         "Getting Started" => "getting-started.md"
-    ]
+    ],
 )
 
-deploydocs(
-    repo = "github.com/n1developer-ubt/data-preprocessing-juml.git",
+deploydocs(;
+    repo = "github.com/n1developer-ubt/data-preprocessing-juml",
     devbranch = "main",
 )
---
+++ docs/src/index.md
@@ -0,0 +1,14 @@
+```@meta
+CurrentModule = PreprocessingPipeline
+```
+
+# PreprocessingPipeline
+
+Documentation for [PreprocessingPipeline](https://github.com/n1developer-ubt/data-preprocessing-juml.git).
+
+```@index
+```
+
+```@autodocs
+Modules = [Preprocessing_Pipeline_JuML]
+```
